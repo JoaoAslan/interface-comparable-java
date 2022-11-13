b2dd77850a28215bd7e617385473ead370996cc2
JoaoAslan <contato.joaoaslan@gmail.com>
2022-11-13 11:03:29 -0300

    Problem with ERROR

--- application/Comparable.java
+++ application/Comparable.java
@@ -1,5 +1,7 @@
 package aula4.application;
 
+import aula4.entities.Employee;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,23 +12,31 @@ import java.util.List;
 public class Comparable {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+        List<Employee> list = new ArrayList<>();
         String path = "D:\\MeusCodigos\\Java\\Interfaces\\src\\aula4\\assets\\names.txt";
 
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             // LENDO O ARQUIVO
-            String name = br.readLine();
-            while (name != null) {
-                list.add(name);
-                name = br.readLine();
+            String employeeCSV = br.readLine();
+            while (employeeCSV != null) {
+                // RECORTANDO OS DADOS (0, 1)
+                String[] fields = employeeCSV.split(",");
+                String name = fields[0];
+                Double salary = Double.parseDouble(fields[1]);
+
+                list.add(new Employee(name, salary));
+
+                employeeCSV = br.readLine();
             }
 
-            // ORDENANDO UMA COLEÇÃO
+            /* ORDENANDO UMA COLEÇÃO
             Collections.sort(list);
+            ERROR: pois a classe Employee não é um tipo Comparable!
+            */
 
             // PRINTANDO CADA VALOR DA LISTA
-            for (String s : list) {
-                System.out.println(s);
+            for (Employee emp : list) {
+                System.out.println(emp);
             }
 
         }
